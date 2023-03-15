be86a8445d1164b532b64e0b28aa9aefb9ccf5a6
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-03-15 20:46:01 +0400

    Style the button when the task is marked as important

--- src/modules/domController.js
+++ src/modules/domController.js
@@ -16,8 +16,10 @@ const domController = (() => {
         }" 
         data-id="${task.id}"
       >
-        <div class="task-item-check">
-          <input type="checkbox" ${task.isDone ? ' checked' : ''}/>
+        <div class="task-item-checkbox">
+          <input type="checkbox" id="task-checkbox" ${
+            task.isDone ? ' checked' : ''
+          }/>
           <p>${task.title}</p>
         </div>
         <div class="task-date-container">
@@ -34,8 +36,12 @@ const domController = (() => {
           <button title="Show info" id="show-task-info">
             <i class="fa-solid fa-circle-info fa-fw"></i>
           </button>
-          <button title="Mark as important" id="mark-important-task">
-            <i class="fa-regular fa-star fa-fw"></i>
+          <button title="Mark as important" id="mark-important-btn">
+            <i class=" ${
+              task.isImportant === true
+                ? 'fa-solid fa-star fa-fw important'
+                : 'fa-regular fa-star fa-fw'
+            }"></i>
           </button>
           <button title="Delete" id="delete-task">
             <i class="fa-solid fa-trash-can fa-fw"></i>
@@ -49,23 +55,34 @@ const domController = (() => {
     return taskItem;
   };
 
-  const editTask = () => {};
+  const deleteTask = (task) => {
+    task.remove();
+  };
 
-  const deleteTask = () => {};
+  const markTaskImportant = (taskBtn, ...rest) => {
+    const [status] = rest;
+    if (status) {
+      taskBtn.classList.remove('fa-regular');
+      taskBtn.classList.add('important', 'fa-solid');
+    } else {
+      taskBtn.classList.remove('important', 'fa-solid');
+      taskBtn.classList.add('fa-regular');
+    }
+  };
 
   const createProject = (project) => {
     const projectItem = String.raw`
       <div class="project-item" data-id="${project.id}">
         <div class="project-info">
-          <p>Project Name</p>
+          <p>${project.title}</p>
         </div>
         <div class="project-menu">
-          <button>
+          <button id="project-menu-btn">
             <i class="fa-solid fa-ellipsis-vertical"></i>
           </button>
-          <div class="project-popup-menu">
-            <button>Edit</button>
-            <button>Delete</button>
+          <div class="project-popup-menu" id="project-menu">
+            <button id="projectEditBtn">Edit</button>
+            <button id="projectDeleteBtn">Delete</button>
           </div>
         </div>
       </div>
@@ -74,31 +91,39 @@ const domController = (() => {
     return projectItem;
   };
 
-  const editProject = () => {};
-
   const deleteProject = () => {};
 
-  const populateTaskContainer = (taskArray) => {
+  const populateTaskContainer = (taskCollection) => {
     taskContainer.textContent = '';
-    taskArray.forEach((task) => {
+    taskCollection.forEach((task) => {
       taskContainer.innerHTML += createTask(task);
     });
   };
 
   const populateProjectContainer = (projectCollection) => {
     projectContainer.textContent = '';
-    projectArray.forEach((project) => {
+    projectCollection.forEach((project) => {
       projectContainer.innerHTML += createProject(project);
     });
   };
 
+  const setCurrentFolder = (e) => {
+    const selectedFolder = e.target;
+    const folderID = selectedFolder.getAttribute('id');
+
+    // const unselectAllFolder = () => {};
+
+    // if (folderID === 'inbox') {
+    //   selectedFolder;
+    // }
+  };
+
   return {
-    editTask,
     deleteTask,
     createTask,
-    editProject,
     createProject,
     deleteProject,
+    markTaskImportant,
     populateTaskContainer,
     populateProjectContainer,
   };
