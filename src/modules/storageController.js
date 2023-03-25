3fa2e0ec7f9803b9e3812dde38f96b18fa9d1f95
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-03-25 23:38:16 +0400

    Write a module to work with the local storage
    The module saves tasks and projects to the local storage and reads from it when page is opened or refreshed

---
+++ src/modules/storageController.js
@@ -0,0 +1,33 @@
+const storageController = (() => {
+  const saveToLocalStorage = (tasks, projects) => {
+    // save items to local storage
+    const taskCollection = JSON.stringify(tasks);
+    const projectCollection = JSON.stringify(projects);
+
+    localStorage.setItem('taskCollection', taskCollection);
+    localStorage.setItem('projectCollection', projectCollection);
+    console.log('success');
+  };
+
+  let retrievedTaskCollection = [];
+  let retrievedProjectCollection = [];
+
+  const readFromLocalStorage = () => {
+    // get items from the local storage
+    retrievedTaskCollection = JSON.parse(
+      localStorage.getItem('taskCollection')
+    );
+    retrievedProjectCollection = JSON.parse(
+      localStorage.getItem('projectCollection')
+    );
+  };
+
+  return {
+    saveToLocalStorage,
+    readFromLocalStorage,
+  };
+})();
+
+export default storageController;
+
+// TODO: save collections each time changes are made
