e78fdcd1f3a7c8a021ba16ea30f784b0a7c51bd8
Elmir Khalilov <edik.halilov1999@gmail.com>
2023-03-25 23:47:45 +0400

    Check the local storage onload
    If there is any project or task in the local storage load them, if not create a default project and task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import './styles/main.sass';
+import handlersController from './modules/handlersController';
+import dataController from './modules/dataController';
+import domController from './modules/domController';
+import storageController from './modules/storageController';
+
+const createDefaultProject = () => {
+  const defaultTestCollection = [
+    {
+      title: 'test task',
+      id: '000000001',
+      project: '00001',
+      description: 'write anything you want',
+      priority: 'medium',
+      dueDate: '',
+      isImportant: true,
+      isDone: false,
+    },
+  ];
+  const defaultProjectCollection = [{ title: 'test project', id: '00001' }];
+
+  dataController.setTaskCollection(defaultTestCollection);
+  dataController.setProjectCollection(defaultProjectCollection);
+  domController.populateTaskContainer(dataController.getTaskCollection());
+  domController.populateProjectContainer(dataController.getProjectCollection());
+};
+
+const checkLocalStorage = () => {
+  let tasks = JSON.parse(localStorage.getItem('taskCollection'));
+  let projects = JSON.parse(localStorage.getItem('projectCollection'));
+  if (tasks !== null || projects !== null) {
+    dataController.setTaskCollection(tasks);
+    dataController.setProjectCollection(projects);
+    console.table(dataController.getTaskCollection());
+    console.table(dataController.getProjectCollection());
+    domController.populateTaskContainer(dataController.getTaskCollection());
+    domController.populateProjectContainer(
+      dataController.getProjectCollection()
+    );
+  } else {
+    createDefaultProject();
+  }
+};
+
+checkLocalStorage();
